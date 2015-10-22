f7529e53fb97d2461704e5005b6e2e18f4a0b639
Brian Schaab <schaab.brian@gmail.com>
2015-10-22 18:51:54 -0500

    Adds csreg.sv a file for registers that store control_words

---
+++ csreg.sv
@@ -0,0 +1,35 @@
+import lc3b_types::*;
+
+
+module csreg #(parameter width = 24)
+(
+    input clk,
+    input load,
+    input lc3b_control_word in,
+    output lc3b_control_word out
+);
+
+lc3b_control_word data;
+
+/* Altera device registers are 0 at power on. Specify this
+ * so that Modelsim works as expected.
+ */
+initial
+begin
+    data = 1'b0;
+end
+
+always_ff @(posedge clk)
+begin
+    if (load)
+    begin
+        data = in;
+    end
+end
+
+always_comb
+begin
+    out = data;
+end
+
+endmodule : csreg
