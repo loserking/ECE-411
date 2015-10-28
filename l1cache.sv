10368efa87f0dbf37f5a85510ed8994fd0070486
Brian Schaab <schaab.brian@gmail.com>
2015-10-28 17:45:35 -0500

    Adds l1icache and l1dcache to mp3.sv

---
+++ l1cache.sv
@@ -0,0 +1,125 @@
+/* cache design top level -- contains the cache controller and the cache datapath */
+import lc3b_types::*;
+
+module l1cache
+(
+    input clk,
+	 input cache_line pmem_rdata,
+	 input logic pmem_resp,
+	 input lc3b_mem_wmask mem_byte_enable,
+	 input lc3b_word mem_address,
+	 input lc3b_word mem_wdata,
+	 input mem_read,
+    input mem_write,
+
+
+    /* Memory signals */
+    output mem_resp,
+    output lc3b_word mem_rdata,
+
+
+	 output logic pmem_read,
+	 output logic pmem_write,
+	 output lc3b_word pmem_address,
+	 output cache_line pmem_wdata
+	 
+);
+
+
+/*internal signals*/
+
+logic hit;
+logic lru_out;
+logic data0write;
+logic data1write;
+logic valid0write;
+logic valid1write;
+logic tag0write;
+logic tag1write;
+logic lru_in;
+logic way0and_out;
+logic lru_write;
+logic dirtymux_out;
+logic stbwritemux_sel;
+logic rwmux_sel;
+logic dirty0write;
+logic dirty1write;
+logic dirty0_in;
+logic dirty1_in;
+logic pmemmux_sel;
+
+
+cache_control cache_control
+(
+	 .clk,
+    .mem_address(mem_address),
+	 .mem_byte_enable(mem_byte_enable),
+	 .mem_write(mem_write),
+	 .mem_read(mem_read),
+	 .mem_wdata(mem_wdata),
+	 .lru_out(lru_out),
+	 .lru_in(lru_in),
+	 .way0and_out(way0and_out),
+	 .lru_write(lru_write),
+	 
+     
+     /* Datapath controls */
+    .pmem_rdata(pmem_rdata),
+	 .pmem_resp(pmem_resp),
+	 .dirtymux_out(dirtymux_out),
+	 .hit(hit),
+	 
+	 .pmemmux_sel(pmemmux_sel),
+	 .rwmux_sel(rwmux_sel),
+	 .stbwritemux_sel(stbwritemux_sel),
+	 .mem_resp(mem_resp),
+    .pmem_read(pmem_read),
+	 .data0write(data0write),
+	 .data1write(data1write),
+	 .tag0write(tag0write), 
+	 .tag1write(tag1write),
+	 .valid0write(valid0write),
+	 .valid1write(valid1write),
+	 .dirty0write(dirty0write),
+	 .dirty1write(dirty1write),
+	 .dirty0_in(dirty0_in),
+	 .dirty1_in(dirty1_in),
+    .pmem_write(pmem_write)
+);
+
+cache_datapath  cache_datapath
+(
+    .clk,
+	 /*inputs from cpu*/
+	 .mem_address(mem_address),
+	 .data0write(data0write),
+	 .data1write(data1write),
+	 .tag0write(tag0write),
+	 .tag1write(tag1write),
+	 .valid0write(valid0write),
+	 .valid1write(valid1write),
+	 .dirty0write(dirty0write),
+	 .dirty1write(dirty1write),
+	 .dirty0_in(dirty0_in),
+	 .dirty1_in(dirty1_in),
+	 .pmem_rdata(pmem_rdata),
+	 .mem_wdata(mem_wdata),
+	 .mem_rdata(mem_rdata),
+	 .mem_byte_enable(mem_byte_enable),
+	 .lru_in(lru_in),
+	 .lru_write(lru_write),
+	 .way0and_out(way0and_out),
+	 .rwmux_sel(rwmux_sel),
+	 .stbwritemux_sel(stbwritemux_sel),
+	 .pmemmux_sel(pmemmux_sel),
+
+    .hit(hit),
+	 .dirtymux_out(dirtymux_out),
+	 .lru_out(lru_out),
+	 .pmem_address(pmem_address),
+	 .pmem_wdata(pmem_wdata)
+);
+
+
+endmodule : l1cache
+
--- mp3.sv
+++ mp3.sv
@@ -3,25 +3,50 @@ import lc3b_types::*;
 module mp3
 (
     input clk,
-	 input lc3b_word i_mem_rdata,
-	 input logic i_mem_resp,
-	 input lc3b_word d_mem_rdata,
-	 input logic d_mem_resp,
-	 
-	 output lc3b_word i_mem_address,
-	 output logic [1:0] i_mem_byte_enable,
-	 output logic i_mem_read,
-	 output logic i_mem_write,
-	 output lc3b_word i_mem_wdata,
-	 output lc3b_word d_mem_address,
-	 output logic [1:0] d_mem_byte_enable,
-	 output logic d_mem_write,
-	 output logic d_mem_read,
-	 output lc3b_word d_mem_wdata
+	 input logic pmem_resp,
+	 input lc3b_word pmem_rdata,
+	 output logic pmem_read,
+	 output logic pmem_write,
+	 output logic [1:0] pmem_byte_enable,
+	 output lc3b_word pmem_wdata,
+	 output lc3b_word pmem_address
 	 
 );
-
-/* Instantiate MP 3 top level blocks here */
+/*Begin internal signals*/
+	/*CPU internal signals*/
+		lc3b_word i_mem_address;
+		lc3b_word i_mem_wdata;
+		logic i_mem_read;
+		logic i_mem_write;
+		logic [1:0] i_mem_byte_enable;
+		lc3b_word i_mem_rdata;
+		logic i_mem_resp;
+		
+		lc3b_word d_mem_address;
+		lc3b_word d_mem_wdata;
+		logic d_mem_read;
+		logic d_mem_write;
+		logic [1:0] d_mem_byte_enable;
+		lc3b_word d_mem_rdata;
+		logic d_mem_resp;
+	/*L1 I cache internal signals*/
+		lc3b_word i_pmem_address;
+		cache_line i_pmem_wdata;
+		logic i_pmem_read;
+		logic i_pmem_write;
+		logic [1:0] i_pmem_byte_enable;
+		cache_line i_pmem_rdata;
+		logic i_pmem_resp;
+	/*L1 D cache internal signals*/
+		lc3b_word d_pmem_address;
+		cache_line d_pmem_wdata;
+		logic d_pmem_read;
+		logic d_pmem_write;
+		logic [1:0] d_pmem_byte_enable;
+		cache_line d_pmem_rdata;
+		logic d_pmem_resp;
+/*End Internal Signals*/
+/*Begin CPU datapath components */
 cpu_datapath cpu_datapath
 (
 	.clk,
@@ -40,7 +65,58 @@ cpu_datapath cpu_datapath
 	.d_mem_rdata(d_mem_rdata),
 	.d_mem_resp(d_mem_resp)
 );
+/*End CPU datapath components*/
 
+/*Begin L1 I-Cache Components*/
 
+l1cache l1icache
+(
+    /*Signals from CPU*/
+    .clk,
+    .mem_address(i_mem_address),
+	 .mem_write(i_mem_write),
+	 .mem_wdata(i_mem_wdata),
+	 .mem_byte_enable(i_mem_byte_enable),
+	 .mem_read(i_mem_read),
+	 
+     
+     /* Datapath controls */
+    .pmem_rdata(i_pmem_rdata),
+	 .pmem_resp(i_pmem_resp),
+
+	 .mem_rdata(i_mem_rdata),
+	 .mem_resp(i_mem_resp),
+    .pmem_read(i_pmem_read),
+    .pmem_write(i_pmem_write),
+	 .pmem_address(i_pmem_address),
+	 .pmem_wdata(i_pmem_wdata)
+);
+
+/*End L1 I-Cache Components*/
+
+/*Begin L1 D-Cache Components*/
+l1cache l1dcache
+(
+    /*Signals from CPU*/
+    .clk,
+    .mem_address(d_mem_address),
+	 .mem_write(d_mem_write),
+	 .mem_wdata(d_mem_wdata),
+	 .mem_byte_enable(d_mem_byte_enable),
+	 .mem_read(d_mem_read),
+	 
+     
+     /* Datapath controls */
+    .pmem_rdata(d_pmem_rdata),
+	 .pmem_resp(d_pmem_resp),
+
+	 .mem_rdata(d_mem_rdata),
+	 .mem_resp(d_mem_resp),
+    .pmem_read(d_pmem_read),
+    .pmem_write(d_pmem_write),
+	 .pmem_address(d_pmem_address),
+	 .pmem_wdata(d_pmem_wdata)
+);
+/*End L1 D-Cache Components*/
 
 endmodule : mp3
